1f18d334879a49f3ccd7ffd7f0dca99df3d485ae
NicoNeko <nicuch100@gmail.com>
2026-02-25 20:36:25 +0200

    Reload on main/global thread

--- core/common/src/main/kotlin/com/willfp/libreforge/commands/CommandReload.kt
+++ core/common/src/main/kotlin/com/willfp/libreforge/commands/CommandReload.kt
@@ -1,7 +1,10 @@
 package com.willfp.libreforge.commands
 
 import com.willfp.eco.core.EcoPlugin
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.toNiceString
 import org.bukkit.command.CommandSender
 
 internal class CommandReload(
@@ -13,8 +16,15 @@ internal class CommandReload(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        @Suppress("UsagesOfObsoleteApi")
-        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
-        plugin.reload(false)
+        val runnable = Runnable {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime(false).toNiceString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable) // run on global thread
+        else
+            runnable.run()
     }
 }
